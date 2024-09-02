fix current calculation

bfield.py
@@ -15,7 +15,7 @@ import numpy as np
 def solution(position=np.zeros(3),mradius=0,mheight=0,magnetization=0,accuracy=[70,30]): # position (v3d coordinates), magnet (radius, height, magnetization)
     v_steps,cir_steps = accuracy[0],accuracy[1]
     field = np.zeros(3)
-    current = magnetization*mheight/v_steps
+    current = 2*np.pi*mradius * magnetization*mheight/v_steps
     cir_step = np.pi*2 / cir_steps
 
     circle = np.linspace(0,2*np.pi,cir_steps)

bfield_threaded.py
@@ -36,7 +36,7 @@ def solution(position=np.zeros(3),mradius=0,mheight=0,magnetization=0,accuracy=[
     global current,cir_step,circle,v_steps,cir_steps,circle,g_mradius,g_mheight, g_position
     g_mradius,g_mheight,g_position = mradius, mheight, position
     v_steps,cir_steps = accuracy[0],accuracy[1]
-    current = magnetization*mheight/v_steps
+    current = 2*np.pi*mradius *magnetization*mheight/v_steps
     cir_step = np.pi*2 / cir_steps
     circle = np.linspace(0,2*np.pi,cir_steps)
 

draw_field.py
@@ -14,7 +14,7 @@ a = 0.005  # radius of the magnet in meters
 b = 0.001  # length of the magnet in meters
 M = 1e5  # magnetization in A/m
 
-grid = 40
+grid = 100
 grid_size = 0.04
 x = np.linspace(-grid_size/2, grid_size/2, grid)
 z = np.linspace(-grid_size/2, grid_size/2, grid)
@@ -44,7 +44,7 @@ fig, ax = plt.subplots(figsize=(10, 10))
 
 # Plot the B-field
 print("Rendering Stream Plot")
-stream = ax.streamplot(X, Z, Bx, Bz, density=2, color=B_mag, cmap='viridis', 
+stream = ax.streamplot(X, Z, Bx, Bz, density=6, color=B_mag, cmap='viridis', 
                        linewidth=1, arrowsize=1, norm=plt.Normalize(vmin=0, vmax=B_mag.max(),),broken_streamlines=True)
 
 # Plot the magnet